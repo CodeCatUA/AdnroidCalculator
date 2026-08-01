7a9c903d3eefe646033120a4700682aa2ed1f334
Student_6 <codecat.ua@gmail.com>
2026-08-01 16:12:21 +0300

    added number buttons code

--- app/src/main/java/com/codecat/adnroidcalculator/MainActivity.java
+++ app/src/main/java/com/codecat/adnroidcalculator/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvDisplay;
+    private boolean bNewNumber = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +26,27 @@ public class MainActivity extends AppCompatActivity {
         });
 
         tvDisplay = findViewById(R.id.textView);
-        findViewById(R.id.btn1)
+
+        findViewById(R.id.btn0).setOnClickListener(v  ->  addDigit("0"));
+        findViewById(R.id.btn1).setOnClickListener(v  ->  addDigit("1"));
+        findViewById(R.id.btn2).setOnClickListener(v  ->  addDigit("2"));
+        findViewById(R.id.btn3).setOnClickListener(v  ->  addDigit("3"));
+        findViewById(R.id.btn4).setOnClickListener(v  ->  addDigit("4"));
+        findViewById(R.id.btn5).setOnClickListener(v  ->  addDigit("5"));
+        findViewById(R.id.btn6).setOnClickListener(v  ->  addDigit("6"));
+        findViewById(R.id.btn7).setOnClickListener(v  ->  addDigit("7"));
+        findViewById(R.id.btn8).setOnClickListener(v  ->  addDigit("8"));
+        findViewById(R.id.btn9).setOnClickListener(v  ->  addDigit("9"));
+
+    }
+
+    private void addDigit(String number) {
+        if (bNewNumber) {
+            tvDisplay.setText(number);
+            bNewNumber = false;
+        } else {
+            tvDisplay.append(number);
+        }
+
     }
 }
